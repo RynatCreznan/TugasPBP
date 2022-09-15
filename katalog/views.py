d68a4412ab6822b1ca2a63624a86514bbe92c5b9
Elang Permana <elang.permana@ui.ac.id>
2022-09-15 09:18:38 +0700

    coba

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+    'name': 'Elang Permana',
+    'student_id': '2006520405',
+    'list_katalog': data_catalog_item
+    }
+    return render(request, 'katalog.html',context)
